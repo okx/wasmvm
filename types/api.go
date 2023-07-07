2352f05aa44af68c079bd4318683ab71b1b486bd
ylsGit <oker@okerdeMacBook-Pro.local>
2023-07-07 14:44:44 +0800

    create wasm contract by contract

--- types/api.go
+++ types/api.go
@@ -3,9 +3,20 @@ package types
 type (
 	HumanizeAddress     func([]byte) (string, uint64, error)
 	CanonicalizeAddress func(string) ([]byte, uint64, error)
+	Contract            func(request ContractCreateRequest, gasLimit uint64) (string, uint64, error)
 )
 
 type GoAPI struct {
 	HumanAddress     HumanizeAddress
 	CanonicalAddress CanonicalizeAddress
+	Contract         Contract
+}
+
+type ContractCreateRequest struct {
+	Creator   string `json:"creator"`
+	WasmCode  []byte `json:"wasm_code"`
+	InitMsg   []byte `json:"init_msg"`
+	AdminAddr string `json:"admin_addr"`
+	Label     string `json:"label"`
+	//Deposit   Coins  `json:"deposit"`
 }
