026f7bc0a81d41ed75395fb1df54c22292d4ef67
lyh169 <liuyuhang203@sina.com>
2023-07-27 17:46:44 +0800

    del unused code

--- types/api.go
+++ types/api.go
@@ -13,15 +13,3 @@ type GoAPI struct {
 	GetCallInfo      GetCallInfo
 	TransferCoins    TransferCoins
 }
-
-type ContractCreateRequest struct {
-	Creator   string `json:"creator"`
-	WasmCode  []byte `json:"wasm_code"`
-	CodeID    uint64 `json:"code_id"`
-	InitMsg   []byte `json:"init_msg"`
-	AdminAddr string `json:"admin_addr"`
-	Label     string `json:"label"`
-	IsCreate2 bool   `json:"is_create2"`
-	Salt      []byte `json:"salt"`
-	//Deposit   Coins  `json:"deposit"`
-}
