eafd6ae256751ea3ea0c72c3a756a1cecd1b8cbc
JianGuo <zjg555543@163.com>
2023-08-01 17:43:58 +0800

    fix gas (#20)
    * branch = "optimize-all"
    
    * optimize-all
    
    * fix gas
    
    * recover code
    
    * update
    
    ---------
    
    Co-authored-by: chunfengSun <516108736@qq.com>
    Co-authored-by: chengzhinei <chengxue5yuan@163.com>

--- internal/api/lib_test.go
+++ internal/api/lib_test.go
@@ -360,7 +360,7 @@ func TestInstantiate(t *testing.T) {
 	res, cost, err := Instantiate(cache, checksum, env, info, msg, &igasMeter, store, api, &querier, TESTING_GAS_LIMIT, TESTING_PRINT_DEBUG)
 	require.NoError(t, err)
 	requireOkResponse(t, res, 0)
-	assert.Equal(t, uint64(0x13a78a36c), cost)
+	assert.Equal(t, uint64(0x13803369c), cost)
 
 	var result types.ContractResult
 	err = json.Unmarshal(res, &result)
@@ -391,7 +391,7 @@ func TestExecute(t *testing.T) {
 	diff := time.Since(start)
 	require.NoError(t, err)
 	requireOkResponse(t, res, 0)
-	assert.Equal(t, uint64(0x13a78a36c), cost)
+	assert.Equal(t, uint64(0x13803369c), cost)
 	t.Logf("Time (%d gas): %s\n", cost, diff)
 
 	// execute with the same store
@@ -404,7 +404,7 @@ func TestExecute(t *testing.T) {
 	res, cost, err = Execute(cache, checksum, env, info, []byte(`{"release":{}}`), &igasMeter2, store, api, &querier, TESTING_GAS_LIMIT, TESTING_PRINT_DEBUG)
 	diff = time.Since(start)
 	require.NoError(t, err)
-	assert.Equal(t, uint64(0x222892d70), cost)
+	assert.Equal(t, uint64(0x22013c0a0), cost)
 	t.Logf("Time (%d gas): %s\n", cost, diff)
 
 	// make sure it read the balance properly and we got 250 atoms
@@ -512,7 +512,7 @@ func TestExecuteCpuLoop(t *testing.T) {
 	diff := time.Since(start)
 	require.NoError(t, err)
 	requireOkResponse(t, res, 0)
-	assert.Equal(t, uint64(0xd45091d0), cost)
+	assert.Equal(t, uint64(0xddcab790), cost)
 	t.Logf("Time (%d gas): %s\n", cost, diff)
 
 	// execute a cpu loop
@@ -663,7 +663,7 @@ func TestMultipleInstances(t *testing.T) {
 	require.NoError(t, err)
 	requireOkResponse(t, res, 0)
 	// we now count wasm gas charges and db writes
-	assert.Equal(t, uint64(0x138559c5c), cost)
+	assert.Equal(t, uint64(0x135e02f8c), cost)
 
 	// instance2 controlled by mary
 	gasMeter2 := NewMockGasMeter(TESTING_GAS_LIMIT)
@@ -674,14 +674,14 @@ func TestMultipleInstances(t *testing.T) {
 	res, cost, err = Instantiate(cache, checksum, env, info, msg, &igasMeter2, store2, api, &querier, TESTING_GAS_LIMIT, TESTING_PRINT_DEBUG)
 	require.NoError(t, err)
 	requireOkResponse(t, res, 0)
-	assert.Equal(t, uint64(0x1399177bc), cost)
+	assert.Equal(t, uint64(0x1371c0aec), cost)
 
 	// fail to execute store1 with mary
-	resp := exec(t, cache, checksum, "mary", store1, api, querier, 0x1218ff5d0)
+	resp := exec(t, cache, checksum, "mary", store1, api, querier, 0x11f1a8900)
 	require.Equal(t, "Unauthorized", resp.Err)
 
 	// succeed to execute store1 with fred
-	resp = exec(t, cache, checksum, "fred", store1, api, querier, 0x22188d470)
+	resp = exec(t, cache, checksum, "fred", store1, api, querier, 0x21f1367a0)
 	require.Equal(t, "", resp.Err)
 	require.Equal(t, 1, len(resp.Ok.Messages))
 	attributes := resp.Ok.Attributes
@@ -690,7 +690,7 @@ func TestMultipleInstances(t *testing.T) {
 	require.Equal(t, "bob", attributes[1].Value)
 
 	// succeed to execute store2 with mary
-	resp = exec(t, cache, checksum, "mary", store2, api, querier, 0x2220900f0)
+	resp = exec(t, cache, checksum, "mary", store2, api, querier, 0x21f939420)
 	require.Equal(t, "", resp.Err)
 	require.Equal(t, 1, len(resp.Ok.Messages))
 	attributes = resp.Ok.Attributes
