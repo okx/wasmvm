ba4e56fd22b24099af0ca79d80a649667b2ca334
JianGuo <zjg555543@163.com>
2023-08-01 16:27:23 +0800

    upgrade wasm40 (#21)
    * branch = "optimize-all"
    
    * optimize-all
    
    * upgrade wasm40
    
    * update
    
    ---------
    
    Co-authored-by: chunfengSun <516108736@qq.com>
    Co-authored-by: chengzhinei <chengxue5yuan@163.com>

--- internal/api/bindings.h
+++ internal/api/bindings.h
@@ -354,6 +354,15 @@ struct cache_t *init_cache(struct ByteSliceView data_dir,
                            uint32_t instance_memory_limit,
                            struct UnmanagedVector *error_msg);
 
+void update_cur_block_num(struct cache_t *cache,
+                          uint64_t cur_block_num,
+                          struct UnmanagedVector *error_msg);
+
+void update_milestone(struct cache_t *cache,
+                      struct ByteSliceView milestone,
+                      uint64_t block_num,
+                      struct UnmanagedVector *error_msg);
+
 struct UnmanagedVector save_wasm(struct cache_t *cache,
                                  struct ByteSliceView wasm,
                                  bool unchecked,
